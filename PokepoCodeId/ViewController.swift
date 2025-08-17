0b574cd038bfc8e533b18a5b3e97920a3f42fbb1
Ebod <rahmanarif.9764@gmail.com>
2025-08-18 00:36:15 +0700

    Initial Commit

---
+++ PokepoCodeId/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokepoCodeId
+//
+//  Created by Arif Rahman Mac Mini on 18/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PokepoCodeIdUITests/PokepoCodeIdUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  PokepoCodeIdUITestsLaunchTests.swift
+//  PokepoCodeIdUITests
+//
+//  Created by Arif Rahman Mac Mini on 18/08/25.
+//
+
+import XCTest
+
+final class PokepoCodeIdUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
